ac037ac89706e94314ae6071ba210d53a13fb366
Adam Letenyei <35657485+ProjectPannonia@users.noreply.github.com>
2020-04-02 13:47:08 +0200

    Computers behaviour implemented

--- src/com/company/factory/modells/Computer.java
+++ src/com/company/factory/modells/Computer.java
@@ -1,5 +1,15 @@
 package com.company.factory.modells;
 
 public abstract class Computer {
+    public abstract String getCpu();
+    public abstract String getRam();
+    public abstract String getHdd();
+    public abstract String getSsd();
+    public abstract String getVideoCard();
 
+    public String toString(){
+       return  "Cpu: " + this.getCpu() + ", Ram: " + this.getRam() +
+               ", Hdd: " + this.getHdd() + ", Ssd: " + this.getSsd() +
+               ", Video Card: " + this.getVideoCard() + ".";
+    }
 }
--- src/com/company/factory/modells/SuperComputer.java
+++ src/com/company/factory/modells/SuperComputer.java
@@ -1,4 +1,42 @@
 package com.company.factory.modells;
 
-public class SuperComputer {
+public class SuperComputer extends Computer{
+    private String cpu;
+    private String ram;
+    private String hdd;
+    private String ssd;
+    private String videoCard;
+
+    public SuperComputer(String cpu, String ram, String hdd, String ssd, String videoCard) {
+        this.cpu = cpu;
+        this.ram = ram;
+        this.hdd = hdd;
+        this.ssd = ssd;
+        this.videoCard = videoCard;
+    }
+
+    @Override
+    public String getCpu() {
+        return this.cpu;
+    }
+
+    @Override
+    public String getRam() {
+        return this.ram;
+    }
+
+    @Override
+    public String getHdd() {
+        return this.hdd;
+    }
+
+    @Override
+    public String getSsd() {
+        return this.ssd;
+    }
+
+    @Override
+    public String getVideoCard() {
+        return this.videoCard;
+    }
 }
