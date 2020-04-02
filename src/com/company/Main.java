0d32bd426959ce5b7bf2d1328be751cb2f17774c
Adam Letenyei <35657485+ProjectPannonia@users.noreply.github.com>
2020-04-02 13:53:52 +0200

    ComputerFactory behaviour implemented

--- src/com/company/Main.java
+++ src/com/company/Main.java
@@ -1,7 +1,11 @@
 package com.company;
 
+import com.company.factory.ComputerFactory;
+import com.company.factory.modells.Computer;
+
 public class Main {
 
     public static void main(String[] args) {
+        Computer pc = ComputerFactory
     }
 }
--- src/com/company/factory/ComputerFactory.java
+++ src/com/company/factory/ComputerFactory.java
@@ -1,5 +1,23 @@
 package com.company.factory;
 
+import com.company.factory.modells.Computer;
+import com.company.factory.modells.Pc;
+import com.company.factory.modells.Server;
+import com.company.factory.modells.SuperComputer;
+import com.company.factory.types.ComputerTypes;
+
 public class ComputerFactory {
-    
+    public static Computer createComputerForMe(ComputerTypes type, String cpu,String ram,String hdd,String ssd,String videoCard){
+        Computer computer = null;
+
+        if(type.equals("PC")){
+            computer = new Pc(cpu,ram,hdd,ssd,videoCard);
+        }else if(type.equals("SERVER")){
+            computer = new Server(cpu,ram,hdd,ssd,videoCard);
+        }else if(type.equals("SUPERCOMPUTER")){
+            computer = new SuperComputer(cpu,ram,hdd,ssd,videoCard);
+        }
+
+        return computer;
+    }
 }
